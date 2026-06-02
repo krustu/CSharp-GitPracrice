e6f79e9fb8194aabc48b518b3e4e05f107535203
Krustu <kuban.ru86@gmail.com>
2026-06-03 04:53:52 +0600

    Changed Main code

--- Program.cs
+++ Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        Test s1 = new Test();
+        s1.Name = "John";
+        s1.Age = 30;
+        s1.DisplayInfo();
+
+        Test s2 = new Test("Alice", 20);
+        s2.DisplayInfo();
     }
+class Test
+{
+    private string name;
+    private int age;
+    public string Name { get{ return name; } set { name = value; } } 
+    public int Age { get{ return age; } set { age = value; } }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+    
 }
