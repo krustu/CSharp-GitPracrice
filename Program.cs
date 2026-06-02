beba39ea438c61c3f2e4fec09c646e2aa4385558
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-06-02 23:16:54 +0000

    Finally fixed bugs

--- Program.cs
+++ Program.cs
@@ -14,16 +14,25 @@ class Program
         Test s2 = new Test("Alice", 20);
         s2.DisplayInfo();
     }
+}
 class Test
 {
     private string name;
     private int age;
     public string Name { get{ return name; } set { name = value; } } 
     public int Age { get{ return age; } set { age = value; } }
-
+    public Test (string name , int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    public Test ()
+        {
+            this.name = "";
+            this.age = 0;
+        }
     public void DisplayInfo()
     {
         Console.WriteLine($"Name: {Name}, Age: {Age}");
     }
-    
 }
