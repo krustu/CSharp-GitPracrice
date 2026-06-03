1e79b04d17fded766fe4b4c0ca831746fb8ca31f
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-06-03 21:01:33 +0000

    OOP basic training

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,2 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+class Program
+{
+    static void Main()
+    {
+        //Animal animal = new Animal();
+        //animal.Speak();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        Animal bird = new Bird();
+        cat.Speak();
+        bird.Speak();
+        dog.Speak();
+        
+        
+    }
+}
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("animal sound");
+    }
+}
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        //base.Speak();
+        Console.WriteLine("mew mew");
+    }
+}
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        //base.Speak();
+        Console.WriteLine("Gav Gav");
+    }
+}
+class Bird : Animal
+{
+    public override void Speak()
+    {
 
+        Console.WriteLine("tik tik");
+    }
+}
