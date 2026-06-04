019c46699951ddd6aa7abd15e1c90d70e95da12e
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-06-04 22:15:07 +0000

    Finally understood how loop and OOP works

--- Practice2/Program.cs
+++ Practice2/Program.cs
@@ -7,27 +7,38 @@ class Practice2
     private static List<Vehicle> allvehicle = new List<Vehicle>(); 
     static void Main()
     {
-        Console.WriteLine("Meduim Level");
-        allvehicle.Add(new Car());
-        allvehicle.Add(new Plane());
-        allvehicle.Add(new Boat());
+        Console.WriteLine("Medium Level");
 
-        Vehicle transp = new Vehicle();
-        
+        Vehicle car1 = new Car { Name = "Toyota", MaxSpeed = 120 };
+        Vehicle plane1 = new Plane { Name = "Boeing", MaxSpeed = 600 };
+        Vehicle boat1 = new Boat { Name = "Speedboat", MaxSpeed = 80 };
+
+        allvehicle.Add(car1);
+        allvehicle.Add(plane1);
+        allvehicle.Add(boat1);
+
+     
+Console.ReadKey();
+        Console.WriteLine("Using for loop");
         for (int i = 0; i < allvehicle.Count ; i++ )
         {
+            Console.ReadKey();
             allvehicle[i].Move();
         }
+
+      /*  Console.WriteLine("Using foreach loop");
+Console.ReadKey();
+        foreach(var a in allvehicle)
+        {
+            a.Move();
+        } */
     }
 }
 
 class Vehicle
 {
-    public string Name
-    {
-         get;
-        set;
-    }
+    public string Name {get ; set;}
+   
     public int MaxSpeed
     {
         get;
@@ -36,7 +47,7 @@ class Vehicle
     
     public virtual void Move()
     {
-        
+       Console.WriteLine($"{Name} is moving at {MaxSpeed} mph"); 
     }
 
 
@@ -46,7 +57,8 @@ class Car : Vehicle
 {
     public override void Move()
     {
-        Console.WriteLine("Car is driving");
+        base.Move();
+       
     }
 
 }
@@ -54,15 +66,16 @@ class Plane : Vehicle
 {
     public override void Move()
     {
-        Console.WriteLine("Plane is flying");
-        
+        base.Move();
+       
     }
 }
 class Boat : Vehicle
 {
     public override void Move()
     {
-       // base.Move();
-       Console.WriteLine("boat is floating");
+        base.Move();
+       
+      
     }
 }
