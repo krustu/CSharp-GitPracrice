a2c28e06b8c281f870a66914e28f50f394a24402
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-06-14 20:21:42 +0000

    start new practice OOP minin rpg

---
+++ SystemRPG/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class RPG
+{
+    List<Character> npc = new List<Character>();
+    static void Main()
+    {
+        
+    }
+    
+}
+class Character
+{
+    public string ?Name{get ; set; }
+    public int HP{get; set;}
+    public float Damage{get; set;}
+    public int Level{get; set;}
+
+     public virtual void atk( Character target)
+    {
+        
+    }
+     public virtual void takedmg( int Damage )
+    {
+        this.Damage = Damage;
+    }
+
+}
+
+
+
+class Goblins : Character
+{
+    
+}
+class Humans : Character
+{
+    
+}
+class Ogrs : Character
+{
+    
+}
