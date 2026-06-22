57623c7455bf1c8e1b6c26100838bd2f92a9bafd
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-06-22 20:19:18 +0000

    fix bugs

--- SystemRPG/Program.cs
+++ SystemRPG/Program.cs
@@ -13,16 +13,18 @@ class Character
 {
     public string ?Name{get ; set; }
     public int HP{get; set;}
-    public float Damage{get; set;}
+    public int Damage{get; set;}
     public int Level{get; set;}
 
      public virtual void atk( Character target)
     {
-        
+        Console.WriteLine($"{Name} attacks {target.Name} for {Damage} damage.");
+        target.takedmg((int)Damage);
     }
      public virtual void takedmg( int Damage )
     {
         this.Damage = Damage;
+        HP -= Damage;
     }
 
 }
@@ -31,7 +33,14 @@ class Character
 
 class Goblins : Character
 {
-    
+    class Boss : Goblins
+    {
+        
+    }
+    class MaxGoblins : Goblins
+    {
+        
+    }
 }
 class Humans : Character
 {
