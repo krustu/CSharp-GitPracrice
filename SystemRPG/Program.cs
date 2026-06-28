d26afe34027cbd18084ef48639b374b3d6841a80
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-06-28 14:11:20 +0000

    Add goblin boss proporties

--- SystemRPG/Program.cs
+++ SystemRPG/Program.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.IO.Pipes;
 
 class RPG
 {
-    List<Character> npc = new List<Character>();
+    public static List<Character> npc = new List<Character>();
     static void Main()
     {
+        // more browlers
+        Goblins g1 = new Goblins
+        {
+            Name = "Kiwi",
+            HP = 100,
+            Damage = 10,
+            Level = 1
+        };
+
+        npc.Add(g1);
+
+        Goblins g2 = new Goblins
+        {
+            Name = "sally",
+            HP = 100,
+            Damage = 10,
+            Level = 1
+        };
+        npc.Add(g2);
+
+
+
+        // fight 
+        // unique mechanice 
+
+        g1.atk(g2);
+        g2.showstatus();
         
     }
     
@@ -18,7 +46,7 @@ class Character
 
      public virtual void atk( Character target)
     {
-        Console.WriteLine($"{Name} attacks {target.Name} for {Damage} damage.");
+        Console.WriteLine($"{Name} attacks {target.Name} with {Damage} damage.");
         target.takedmg((int)Damage);
     }
      public virtual void takedmg( int Damage )
@@ -26,16 +54,28 @@ class Character
         this.Damage = Damage;
         HP -= Damage;
     }
-
+     public virtual void showstatus()
+    {
+        Console.WriteLine($"Name-{Name} ");
+        Console.WriteLine($"HP-{HP} ");
+        Console.WriteLine($"Damage-{Damage}");
+        Console.WriteLine($"LVl-{Level}");
+    }
+     
 }
 
 
 
 class Goblins : Character
 {
+    //chance to hide attack 
     class Boss : Goblins
     {
-        
+       public override void takedmg( int Damage)
+        {
+            Damage /= 3;
+            base.takedmg(Damage);
+        }
     }
     class MaxGoblins : Goblins
     {
@@ -44,9 +84,13 @@ class Goblins : Character
 }
 class Humans : Character
 {
-    
+    // uniqe power 
+    // ability to improve skills
+    // another skills
+
 }
 class Ogrs : Character
 {
-    
+    //extra hp
+    //weakness from some peaples
 }
