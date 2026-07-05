7027142a95acbfebce912d15664b8d5e1f829f27
Rysbek (KRustu_) <127596193+krustu@users.noreply.github.com>
2026-07-05 17:40:31 +0000

    additional task Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Interface
+{
+    static void Main()
+    {
+         Character MyNPC = new Character();
+         
+    }
+}
+interface IAttacker
+{
+    int AttackPower { get; }
+    void Attack(IDamageable target);
+}
+
+interface IDamageable
+{
+    int Health { get; set; }
+    void TakeDamage(int amount);
+}
+
+class Character : IAttacker, IDamageable
+{
+    public int Health { get; set; } = 100;
+    public int AttackPower { get; set; } = 15;
+
+    public void Attack(IDamageable target) => target.TakeDamage(AttackPower);
+    public void TakeDamage(int amount) => Health -= amount;
+}
